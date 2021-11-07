be31ddd27d800036ffc568507dd4d982c1525f64
YiroYi <yiroyi@live.com>
2021-11-07 11:55:24 -0600

    Adding Input listeners to InputClass

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
+
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,12 +14,32 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+  //bind(this), it is to tell TS that inside submit handler the 'this'
+  //refers to the configure element inside of the ProjectINput class
+  //refers to projectInputClass
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
+
 }
 
 const prjInput = new ProjectInput();
