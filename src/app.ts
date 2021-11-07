0175e7ef4f7920eee95dbb09796a78dac4612b06
YiroYi <yiroyi@live.com>
2021-11-07 13:10:00 -0600

    Adding skeleton for ProjectList

--- src/app.ts
+++ src/app.ts
@@ -53,6 +53,37 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   };
 //Autobind decorator
 
+// ProjectList
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    //the '!' is to tell TS that this variable wont be null
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' Projects';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
+// Project List
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -144,4 +175,7 @@ class ProjectInput {
 
 }
 
+
 const prjInput = new ProjectInput();
+const activePrjList  = new ProjectList('active');
+const finsihedPrjList = new ProjectList('finished');
