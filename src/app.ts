ac88193e79665f3aa31e4b4c6febf7de298a54f0
YiroYi <yiroyi@live.com>
2021-11-07 12:05:29 -0600

    Add 'bind decorator'

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+//Autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn
+    }
+  }
+    return adjDescriptor;
+  };
+//Autobind decorator
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -24,13 +38,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
   //bind(this), it is to tell TS that inside submit handler the 'this'
   //refers to the configure element inside of the ProjectINput class
